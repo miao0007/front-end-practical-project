e9722cbe5939be37e8af7a8b6b818e8efce74ecc
miao0007 <miao188@hotmail.com>
2021-03-30 20:45:18 +1030

    name variable

---
+++ HTML_Music_Player/js/main.js
@@ -0,0 +1,15 @@
+$(
+    function() {
+        var playerTrack = $('.player-track'),
+        albumArt = $('.album-art'),
+        albumName = $('#album-name'),
+        trackName = $('#track-name'),
+        bgArtwork = $('.bg-artwork'),
+        bgArtworkUrl,
+
+        tProgress = $('#current-time'),
+        tTime = $('#track-length'),
+        
+
+    }
+)
